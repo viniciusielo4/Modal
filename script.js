fc908853fda238a64f22b0991083432d9ed2dcdd
Vinicius Ielo Martins de Oliveira <162700647+viniciusielo4@users.noreply.github.com>
2024-09-29 12:34:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var modal = document.getElementById("Modal");
+
+var btn = document.getElementById("botao");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
